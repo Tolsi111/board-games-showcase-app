9dab197c985053f52b5acc37b92b8b461f6374b3
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-07-07 13:58:40 +0300

    moved nav list to header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Home page!</h1>
-      <ul>
-        <li>
-          <Link className={styles.link} href={"/games"}>
-            My games
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.link} href={"/games/suggestion"}>
-            Make a game suggestion
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.link} href={"/about"}>
-            About me
-          </Link>
-        </li>
-      </ul>
     </main>
   );
 }
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,9 +1,29 @@
+import Link from "next/link";
 import styles from "./layout.module.css";
 
 export default function Header() {
   return (
-    <div className={styles.header}>
-      <h1>Header component</h1>
-    </div>
+    <header className={styles.header}>
+      <Link href={"/"}>Board games</Link>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <Link className={styles.link} href={"/games"}>
+              My games
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={"/games/suggestion"}>
+              Make a game suggestion
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href={"/about"}>
+              About me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
