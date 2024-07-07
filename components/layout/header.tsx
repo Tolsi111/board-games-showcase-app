a90f2698302301683181b394947ad80bf7777ffe
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-07-07 13:41:12 +0300

    base component

---
+++ components/layout/header.tsx
@@ -0,0 +1,9 @@
+import styles from "./layout.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Header component</h1>
+    </div>
+  );
+}
