fa5cee7b8457609ff6163758683d7492f93d179c
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-07-08 18:47:22 +0300

    added images to links

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import styles from "@/app/page.module.css";
 
+import wingspan from "@/assets/Wingspan.webp";
+import tfm from "@/assets/TerraformingMars.webp";
+import scythe from "@/assets/Scythe.webp";
+import Image from "next/image";
+
 export default function GamesPage() {
   return (
     <main className={styles.main}>
@@ -9,16 +14,31 @@ export default function GamesPage() {
         <li>
           <Link className={styles.link} href={"/games/wingspan"}>
             Wingspan
+            <Image
+              src={wingspan}
+              alt="Wingspan box artwork"
+              className={styles.gameIcon}
+            />
           </Link>
         </li>
         <li>
           <Link className={styles.link} href={"/games/terraforming-mars"}>
             Terraforming mars
+            <Image
+              src={tfm}
+              alt="Terraforming Mars box artwork"
+              className={styles.gameIcon}
+            />
           </Link>
         </li>
         <li>
           <Link className={styles.link} href={"/games/scythe"}>
             Scythe
+            <Image
+              src={scythe}
+              alt="Scythe box artwork"
+              className={styles.gameIcon}
+            />
           </Link>
         </li>
       </ul>
